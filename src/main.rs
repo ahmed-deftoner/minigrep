1c5156fc6c47fa6d8e734237deee6d968e10d525
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-09-02 14:17:28 +0500

    store args in var

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use std::env;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filepath = &args[2];
+    println!("searching for {}",query);
+    println!("in file {}",filepath);
     dbg!(args);
 }
