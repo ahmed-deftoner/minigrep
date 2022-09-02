2d5fdf72e1961cba14e261085a3a3cc4bf842510
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-09-02 16:57:12 +0500

    search logic

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,13 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str>{
-    return vec![]
+    let mut result = Vec::new();
+    for line in content.lines(){
+        if line.contains(query){
+            result.push(line);
+        }
+    }
+    result
 }
 
 #[cfg(test)]
